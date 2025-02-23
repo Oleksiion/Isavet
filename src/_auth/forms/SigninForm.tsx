4db8d7619f01245dc8dd18279f67cf5b3979f698
iMak <imak@iMac-iMak.local>
2025-02-23 20:37:05 +0200

    create first  structure

---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const SigninForm = () => {
+  return (
+    <>
+      <div>Auth</div>
+      <Button>Click me</Button>
+    </>
+  );
+};
+
+export default SigninForm;
